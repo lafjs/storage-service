d1f822e36f4debdc834354b8eccc694c3b4ec544
maslow <wangfugen@126.com>
2022-04-07 17:33:57 +0800

    init repo

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+/**
+ * @author: lunaragon<sunlong@88.com>
+ */
+export const Constants = {
+  /**
+   * Collection names
+   */
+  cn: {
+    /** buckets collection name */
+    BUCKETS: "fs_buckets",
+  },
+  DIRECTORY_MIME_TYPE: 'application/x-directory'
+}
